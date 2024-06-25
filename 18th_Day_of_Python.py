8057caad3a56230eacd31449d531017a2a7c9b16
AlexandraC98 <alexandra.ciocirdel@icloud.com>
2024-06-25 13:57:40 +0300

    Create 18th_Day_of_Python.py
    Guess the number 🗡️ 
    
    Homework for when I become smarter: to be able to restrict the user and exit the program if he chooses another nmb instead of [0-1000000]

---
+++ 18th_Day_of_Python.py
@@ -0,0 +1,25 @@
+x=1
+nmb=float(input("Enter a number: "))
+while True:
+  guess=float(input("Guess the number: "))
+  if x==3:
+    print("You have run out of guesses")
+    exit()
+  if guess<0 or guess>1000000:
+    print("Wrong choice")
+    exit()
+  elif guess<nmb:
+    print("Too low")
+    x+=1
+    continue
+  elif guess>nmb and guess<=1000000:
+    print("Too high")
+    x+=1
+    continue
+  elif guess==nmb:
+    print("Correct!")
+    break
+if x==1:
+  print("It took you",x,"attempt to guess the number")
+else:
+  print("It took you",x,"attempts to guess the number")
