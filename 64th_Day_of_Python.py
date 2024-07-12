96f466bb154e6ac22ff2427cddeea24f0ba8a386
Alexandra Ciocirdel <alexandra.ciocirdel@icloud.com>
2024-07-12 15:22:57 +0300

    OOP

---
+++ 64th_Day_of_Python.py
@@ -0,0 +1,65 @@
+class job:
+  name=None
+  salary=None
+  hours=None
+
+  def __init__(self,name,salary,hours):
+    self.name=name
+    self.salary=salary
+    self.hours=hours
+
+  def print(self):
+    print(f"""Job type: {self.name}
+Salary: {self.salary}
+Hours worked: {self.hours}""")
+
+
+class doctor(job):
+  speciality=None
+  years=None
+
+  def __init__(self,salary, hours, speciality,years):
+    self.name="Doctor"
+    self.salary=salary
+    self.hours=hours
+    self.speciality=speciality
+    self.years=years
+
+  def print(self):
+    print(f"""Job type: {self.name}
+Salary: {self.salary}
+Hours worked: {self.hours}
+Speciality: {self.speciality}
+Years of experience: {self.years}""")
+
+
+class teacher(job):
+  subject=None
+  position=None
+
+  def __init__(self,salary,hours,subject,position):
+    self.name="CS Teacher"
+    self.salary=salary
+    self.hours=hours
+    self.subject=subject
+    self.position=position
+
+  def print(self):
+    print(f"""Job type: {self.name}
+Salary: {self.salary}
+Hours worked: {self.hours}
+Suvject: {self.subject}
+Position: {self.position}""")
+
+lawyer=job("Lawyer","Squillions","60")
+lawyer.print()
+
+print()
+
+doc=doctor("Doing very nicely in the minds of ignorants","Definitely >50 per week", "Paedriatitian", "Wasted youth for BS")
+doc.print()
+
+print()
+
+teach=teacher("I bet not enough","All of them","CS","CS Teacher")
+teach.print()
